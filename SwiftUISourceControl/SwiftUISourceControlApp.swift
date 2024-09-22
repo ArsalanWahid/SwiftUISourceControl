d135e4f3fb578ede419c022bd2c83f032b091443
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-09-22 18:09:16 +0500

    Initial Commit

---
+++ SwiftUISourceControl/SwiftUISourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISourceControlApp.swift
+//  SwiftUISourceControl
+//
+//  Created by Arsalan Wahid Asghar on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
